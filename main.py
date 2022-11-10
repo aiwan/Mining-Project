c8100a8b277d8cf8bbafd41f4d7df3bfa8039a33
Andres Iwan <andres.iwan@gmail.com>
2022-11-10 14:42:58 +0200

    Testing commit

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Data Mining Project
+# Web Scraper of Goal.com
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
+def main():
+    """
+    Main function, controls the general logic.
+    :return:
+    """
+    pass
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
